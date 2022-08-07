3d55566ee77a8bc0c922369529985a3a4e8c386b
betterme998 <1248715031@qq.com>
2022-08-07 22:56:43 +0800

    购物车案例

---
+++ 05购物车案例-动态添加类-双向绑定v-model/data/data.js
@@ -0,0 +1,31 @@
+const books = [
+  {
+    id: 1,
+    name: '《计算导论》',
+    data: '2006-9',
+    price: 85.00,
+    count: 1
+  },
+  {
+    id: 2,
+    name: 'UNIX编程艺术',
+    data: '2006-2',
+    price: 59.00,
+    count: 1
+  },
+  {
+    id: 1,
+    name: '《编程珠玑》',
+    data: '2008-10',
+    price: 39.00,
+    count: 1
+  },
+  {
+    id: 1,
+    name: '《代码大全》',
+    data: '2006-3',
+    price: 128.00,
+    count: 1
+  },
+
+]
