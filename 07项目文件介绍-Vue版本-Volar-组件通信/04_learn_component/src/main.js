585bbf36a95f51ebae9011b2478efabf3dbbde9f
betterme998 <1248715031@qq.com>
2022-08-15 23:42:04 +0800

    组件通信

--- 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/main.js
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// 修改App.vue文件，演示不同文件代码
+// import App from "./01_组件的嵌套使用/App.vue";
+import App from "./02_组件通信-父传子/App.vue";
 
 createApp(App).mount('#app')
