0ffee8bbb783772b74ec2e5e13874bfa729b7b60
betterme998 <1248715031@qq.com>
2022-08-16 11:26:14 +0800

    组件通信

--- 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/02_组件通信-父传子/App.vue
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/02_组件通信-父传子/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>AppContent</div>
+  <div>
+    <!-- 1.展示why的个人信息 -->
+  <!-- 并通过属性传递数据 -->
+  <!-- 传过去后子组件接受数据：
+    怎要让他们连在一起呢？：用props接收
+    跳子组件 -->
+    <!-- 当限制number类型时，加上: 让它变成js代码，写18就是数组，写:age="'18'"就是字符串 -->
+  <show-info name="why" :age="18" :height="1.88"></show-info>
+
+  <!-- 2.展示kobe的个人信息 -->
+  <!-- 当我们想要展示其他人信息时，通过app.vue来决定数据，且app.vue是父组件，show-info是子组件 -->
+  <show-info name="kobe" :age="30" :height="1.87"></show-info>
+
+  <!-- 3.展示默认的个人信息 -->
+  <show-info></show-info>
+  </div>
+
 </template>
 
 <script>
+import ShowInfo from "./ShowInfo.vue"
 export default {
-
+  components: {
+    ShowInfo
+  }
 }
 </script>
 
---
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/02_组件通信-父传子/ShowInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="infos">
+    <!-- 大胡子函数可以访问到props里的数据 -->
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <h2>身高：{{height}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    // 接收父组件传递过来的属性
+    // 1.数组语法
+    // props:["name", "age", "height"]
+
+    // 2.对象语法
+    props: {
+      name: {
+        type:String, //限制类型
+        default:'我是默认的name'  //设置默认值
+      }, //限制类型
+      age:{
+        type:Number,
+        default:20
+      },
+      height:{
+        type:Number,
+        default:0
+      },
+      // 默认值为对象，必须写成函数，函数来返回默认值
+      friend: {
+        type:Object,
+        default:() => ({name:'jams'})
+      },
+
+      // 数组类型 default也得写成函数，函数来返回默认值
+      hobbies: {
+        type:Array,
+        default: () => ['篮球','rap','唱跳']
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
