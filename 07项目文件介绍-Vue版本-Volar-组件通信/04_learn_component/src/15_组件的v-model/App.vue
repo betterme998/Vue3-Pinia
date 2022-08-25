e87aa80be92f2767b377884dc53617f3cf493b10
betterme998 <1248715031@qq.com>
2022-08-25 23:00:18 +0800

    一个组件绑定多个v-model

--- 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/15_组件的v-model/App.vue
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/15_组件的v-model/App.vue
@@ -5,24 +5,31 @@
     <!-- 相当于下面 -->
     <!-- <input type="text" :value="message" @input="message = $event.target.value"> -->
 
-    <!-- 2.组件的v-model -->
+    <!-- 2.组件的v-model ：默认的modelValue -->
     <counter v-model="appCounter"></counter>
     <!-- 等同于下面 -->
     <!-- <counter :modelValue = "appCounter" @update:modelValue="appCounter = $event"></counter> -->
+
+    <!-- 3.组件的v-model ：自定义的名称：counter -->
+    <!-- 一个组件使用多个v-model，要自定义名字 默认的是modelValue -->
+    <counter2 v-model:counter="appCounter" v-model:why="appwhy"></counter2>
   </div>
 </template>
 
 <script>
   import Counter from './Counter.vue'
+  import Counter2 from './Counter2.vue'
   export default {
     data() {
       return {
-        appCounter:100
+        appCounter:100,
+        appwhy:'coderwhy'
       }
     },
     components: {
-      Counter
-    }
+    Counter,
+    Counter2
+}
   }
 </script>
 
---
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/15_组件的v-model/Counter2.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Counter: {{ counter }}</h2>
+    <button @click="changeVounter">修改message</button>
+    <hr>
+    <!-- why绑定 -->
+    <h2>Why:{{ why }}</h2>
+    <button @click="changewhy">修改message</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      counter: {
+        type:Number,
+        default:0
+      },
+      why: {
+        type:String,
+        default:''
+      }
+    },
+    // 自定义v-model名字，一个组件可以使用多个v-model
+    emits:["update:counter", "update:why"],
+    methods: {
+      changeVounter() {
+        // 这样写能被v-model监听
+        this.$emit("update:counter",1000)
+      },
+      changewhy() {
+        this.$emit("update:why",'kobi')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
