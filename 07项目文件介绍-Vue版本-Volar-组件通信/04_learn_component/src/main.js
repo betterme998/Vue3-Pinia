059370b9fb723b42125b1d6c07448fc1bed0ac8a
betterme998 <1248715031@qq.com>
2022-08-25 14:26:28 +0800

    webpack代码的分包

--- 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/main.js
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+// import { sum } from './utils/math';
 // 修改App.vue文件，演示不同文件代码
 // import App from "./01_组件的嵌套使用/App.vue";
 // import App from "./05_组件插槽-基本使用/App.vue";
@@ -10,7 +11,13 @@ import { createApp } from 'vue'
 // import App from "./12_动态组件的使用/App.vue";
 import App from "./13_Keep-Alive的使用/App.vue";
 
-
+// 这个最终打包会打包到哪里？ app.js里
+// sum(20, 30)
+// import函数可以让webpack对导入文件进行分包处理
+// 输入npm run build 打包后打开dist文件夹，可以发现通过import函数导入的会进行分包处理
+import("./utils/math").then(res => {
+  res.sum(20, 30)
+})
 
 
 
