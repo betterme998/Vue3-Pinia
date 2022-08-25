030ae7d922494ae15db33a416fc28f2af0e8b7d7
betterme998 <1248715031@qq.com>
2022-08-25 16:13:27 +0800

    组件的v-model注意

---
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/15_组件的v-model/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <!-- 1.input v-model -->
+    <!-- <input v-model="message"> -->
+    <!-- 相当于下面 -->
+    <!-- <input type="text" :value="message" @input="message = $event.target.value"> -->
+
+    <!-- 2.组件的v-model -->
+    <counter v-model="appCounter"></counter>
+    <!-- 等同于下面 -->
+    <!-- <counter :modelValue = "appCounter" @update:modelValue="appCounter = $event"></counter> -->
+  </div>
+</template>
+
+<script>
+  import Counter from './Counter.vue'
+  export default {
+    data() {
+      return {
+        appCounter:100
+      }
+    },
+    components: {
+      Counter
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 07项目文件介绍-Vue版本-Volar-组件通信/04_learn_component/src/15_组件的v-model/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Counter: {{ modelValue }}</h2>
+    <button @click="changeVounter">修改message</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      modelValue: {
+        type:Number,
+        default:0
+      }
+    },
+    emits:["update:modelValue"],
+    methods: {
+      changeVounter() {
+        // 这样写能被v-model监听
+        this.$emit("update:modelValue",1000)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
