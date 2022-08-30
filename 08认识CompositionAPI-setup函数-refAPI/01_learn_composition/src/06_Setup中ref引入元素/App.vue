a6c1c2db0e3a0997ba96dcd7a343f3c782c88e06
betterme998 <1248715031@qq.com>
2022-08-30 16:45:27 +0800

    Setup中ref引入元素（获取元素或组件）

---
+++ 08认识CompositionAPI-setup函数-refAPI/01_learn_composition/src/06_Setup中ref引入元素/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- 1.获取元素 -->
+    <h2 ref="tielRef">我是标题</h2>
+    <button ref="btn">按钮</button>
+
+    <!-- 2.获取组件实例 -->
+    <show-info-vue ref="showInfoRef"></show-info-vue>
+    <button @click="getElements">获取元素</button>
+  </div>
+</template>
+
+<script>
+  import { ref, onMounted } from 'vue';
+  import ShowInfoVue from './ShowInfo.vue';
+  export default {
+    components:{
+      ShowInfoVue
+    },
+    setup() {
+      // template中要使用ref必须先创建，后返回出去。才能在类html标签中使用ref=“”
+      const tielRef = ref() //创建h2的ref
+      const btn = ref()//创建按钮的ref
+      const showInfoRef = ref() //创建组件的ref
+
+      // 获取template中ref的元素要使用生命周期函数
+      onMounted(() => {
+        console.log(tielRef.value);
+        console.log(btn.value);
+        console.log(showInfoRef.value); //proxy{...}
+        console.log(showInfoRef.value.showInfoFoo); //拿到子组件的方法
+      }) 
+
+      // 直接拿拿不到
+      function getElements() {
+        console.log(tielRef.value);
+      }
+      return {
+        tielRef,
+        btn,
+        showInfoRef
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 08认识CompositionAPI-setup函数-refAPI/01_learn_composition/src/06_Setup中ref引入元素/ShowInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>ShowInfo</div>
+</template>
+
+<script>
+  export default {
+    // 不要单独用methods
+    // methods: {
+    //   showInfoFoo() {
+    //     console.log("showInfo foo function");
+    //   }
+    // }
+    setup() {
+      // 在setup中定义函数
+      function showInfoFoo() {
+        console.log("showInfo foo function");
+      }
+      return {
+        showInfoFoo
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
