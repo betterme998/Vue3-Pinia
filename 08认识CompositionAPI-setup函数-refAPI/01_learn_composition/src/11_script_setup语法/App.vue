1783b72e1305cae537ae06f60b22811179ec8e15
betterme998 <1248715031@qq.com>
2022-09-01 23:19:28 +0800

    script_setup的语法defineProps,defineEmits,defineExpose

--- 08认识CompositionAPI-setup函数-refAPI/01_learn_composition/src/11_script_setup语法/App.vue
+++ 08认识CompositionAPI-setup函数-refAPI/01_learn_composition/src/11_script_setup语法/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <h2>Appcontent：{{ message }}</h2>
+    <button @click="changeMessage">修改message</button>
 
     <!-- 导入的组件在script setup语法糖下不用注册，直接使用 -->
-    <Showinfo></Showinfo>
+    <!-- 传值给子组件 -->
+  
+    <Showinfo 
+      age="18"
+      @info-btn-click="infoBtnClick"
+      ref="showInfoRef">
+    </Showinfo>
     <Showinfo></Showinfo>
     <Showinfo></Showinfo>
   </div>
@@ -12,6 +19,7 @@
 <script setup>
   // 语法糖下直接导入组件就能用啦
 import Showinfo from "./Showinfo.vue";
+import { ref, onMounted } from "vue";
   // vue3.2提供了语法糖，代码直接写到script里面，在script标签加上 setup
   // 这样就相当于在setup函数里写代码一样的
   /*
@@ -25,8 +33,33 @@ import Showinfo from "./Showinfo.vue";
       这意味着与普通的script只在组件被首次引入的时候执行一次不同
       <script setup>中的代码会每次组件实例被创建的时候执行
   */ 
-  const message = "hello world"
 
+//  1.所有编写在顶层中的代码，都是默认暴漏给template可以使用 （不用返回）
+  const message = ref("hello world")
+
+// 2.定义响应式数据
+  function changeMessage() {
+    message.value = "你好啊，李银河！"
+  }
+
+// 3.定义props 使用函数defineProps  发送自定义事件使用defineEmits
+// 3.defineProps() 和 defineEmits()
+  // 为了在声明props和eimts选项是获得完整的类型推断支持，我们可以使用defineProps和defineEnits API，他们将自动地在<script setup>中可用
+function infoBtnClick(payload) {
+  // 监听子组件点击事件
+  console.log("监听到子组件的点击:", payload);
+}
+
+// 获取组件实例
+// 4.defineExpose() 
+  const showInfoRef = ref()
+  // 主动调用生命周期里的某个方法
+  // 在语法糖的写法下，当我们通过ref获取组件实例，并且需要使用组件内部方法。
+  // 必须使用defineExpose() 方法暴漏出要使用的方法
+  onMounted(()=>{
+    // 拿到组件实例
+    showInfoRef.value.foo()
+  })
 </script>
 
 <style scoped>
--- 08认识CompositionAPI-setup函数-refAPI/01_learn_composition/src/11_script_setup语法/Showinfo.vue
+++ 08认识CompositionAPI-setup函数-refAPI/01_learn_composition/src/11_script_setup语法/Showinfo.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>Showinfo</div>
+  <div>
+    <div>Showinfo:{{ age }}</div>
+    <button @click="shoeInfoBtnClick">showinfoButton</button>
+  </div>
 </template>
 
 <script setup>
+  // 接收父组件数据
+  // 定义props
+  const props = defineProps({
+    age:{
+      type:Number,
+      default:18
+    }
+  }) 
 
+  // 发送自定义事件
+  const emits = defineEmits(["infoBtnClick"])
+  function shoeInfoBtnClick() {
+    emits("infoBtnClick", "showInfo内部发生了点击")
+  }
+
+  // 定义foo的函数
+    function foo() {
+      console.log("foo function");
+    }
+  // 语法糖的写法中，父组件通过ref获取子组件并要使用foo方法，必须在子组件内暴漏出该方法
+  // 通过defineExpose() 方法
+  defineExpose({
+    foo
+  })
 </script>
 
 <style scoped>
