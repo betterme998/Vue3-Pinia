1e0dcb7dfbb82430c442e71efcfea22a585b242e
betterme998 <1248715031@qq.com>
2022-09-04 10:52:19 +0800

    阶段案例

---
+++ 08认识CompositionAPI-setup函数-refAPI/02_阶段案例练习/01_roomsource_demo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <!-- 1.区域header -->
+    <div class="area-header">
+      <div class="title">{{ highScore.title }}</div>
+      <div class="subtitle">{{ highScore.subtitle }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  // 1.获取数据
+  import highScore from "./data/high_score.json"
+  console.log(highScore);
+</script>
+
+<style lang="less" scoped>
+  // 使用less需要下载 less-loader
+  .app {
+
+  }
+</style>
