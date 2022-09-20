912488685bc33de7fdbd57182bbb8f28a9cd438c
betterme998 <1248715031@qq.com>
2022-09-20 15:26:20 +0800

    网络请求-axios库-安装-特点-请求方式-常见的配置选项-get-post-request请求演示及不同写法

---
+++ 12网络请求库-axios库/01_learn_axios/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+/* 
+.安装axios
+  npm install axios
+.功能特点
+  .在浏览器中发送XMLHttpRequests请求
+  .在node.js中发送http请求
+  .支持Promise API
+  .拦截请求和响应
+  .转换请求和响应数据
+  .等等
+.补充：axios名称的由来？个人理解
+  .没有具体的翻译
+  .axios：ajax i/o system
+
+.axios请求方式
+  .支持多种请求方式 config (配置)
+    .axios(config)
+    .axios.request(config)
+    .axios.get(url[,config])
+    .axios.delete(url[,config])
+    .axios.head(url[,config])
+    .axios.post(url[,data[,config]])
+    .axios.put(url[,data[,config]])
+    .axios.patch(url[,data[,config]])
+  .有时候，我们可能需求同时发送两个请求
+    .使用axios.all,可以放入多个请求的数组
+    .axios.all([])返回的结果是一个数组，使用axios.spread可将数组[res1,res2]展开为res1，res2
+
+.常见的配置选项
+  .请求地址
+    url:'/user'
+  .请求类型
+    method:'get'
+  .请求路径
+    baseURL:'http://www.mt.com/api'
+  .请求前的数据处理
+    transformRequest:[function(data){}]
+  .请求后的数据处理
+    transformResponse:[function(data){}]
+  .自定义的请求头
+    headers:{'x-Requested-With':'XMLHttpRequest'}
+  .URL查询对象
+    params:{id:12}
+  .查询对象序列化函数
+    .paramsSerializer:function(params){}
+  .request body
+    data:{key:'aa'}
+  .超时设置
+    timeout:1000
+*/
+</style>
---
+++ 12网络请求库-axios库/01_learn_axios/src/main.js
@@ -0,0 +1,50 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// 下载完后导入axios
+import axios from 'axios'
+createApp(App).mount('#app')
+
+// 使用axios发送网络请求,request函数传入一个配置
+// 1.发送request请求
+axios.request({
+  // 请求地址
+  url: "http://123.207.32.32:8000/home/multidata",
+  // 默认发送get请求
+  method:"get"
+  // 在外部调用zhen方法
+}).then(res => {
+  console.log(res.data);
+})
+
+// 2.发送get请求(写法一),先传入url，在传入配置
+// axios.get("http://123.207.32.32:9001/lyric?id=500665346").then(res => {
+//   console.log("res:",res.data);
+// })
+// 发送get请求(写法二)
+axios.get("http://123.207.32.32:9001/lyric",{
+  // 配置传入参数,这会拼接到url后面
+  params:{
+    id:500665346
+  }
+}).then(res => {
+  console.log("res:",res.data);
+})
+
+// 3.发送post请求（第一种写法）,接收三个参数,先传入url，在传入data，在传入配置
+// axios.post("http://123.207.32.32:1888/02_param/postjson",{
+//   name: "coderwhy",
+//   password: 123456
+// }).then(res => {
+//   console.log("post请求登入res：",res.data);
+// })
+// 3.发送post请求（第二种写法）
+    axios.post("http://123.207.32.32:1888/02_param/postjson",{
+      // 第二个data参数直接写在配置里面，内部会判断
+      data: {
+        name: "coderwhy",
+        password: 123456
+      }
+    }).then(res => {
+      console.log("post请求登入res：",res.data);
+    })
+
