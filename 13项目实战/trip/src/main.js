49d519ce03617ad18c68b9036d0589814e8b43f6
betterme998 <1248715031@qq.com>
2022-09-22 11:20:24 +0800

    css样式重置-第三方重置-自己定义重置

--- 13项目实战/trip/src/main.js
+++ 13项目实战/trip/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// github上的第三方css样式重置
+import "normalize.css"
+
+// 自己定义的css样式包含-重置样式-公共样式-等
+import "./assets/css/index.css"
 
 createApp(App).mount('#app')
