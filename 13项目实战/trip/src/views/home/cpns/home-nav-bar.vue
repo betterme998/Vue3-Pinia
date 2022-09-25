ee2c5673427610f98db23ef725564cfb40ce37c9
betterme998 <1248715031@qq.com>
2022-09-25 14:40:37 +0800

    首页开发-封装navbar，图片-定位部分样式

---
+++ 13项目实战/trip/src/views/home/cpns/home-nav-bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-bar">
+    <div class="title">旅途</div>
+  </div>
+</template>
+
+<script setup>
+  
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+    height: 46px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #f2f2f2;
+
+    .title {
+      font-size: 16px;
+      font-weight: 600;
+      color: var(--primary-color);
+    }
+  }
+</style>
