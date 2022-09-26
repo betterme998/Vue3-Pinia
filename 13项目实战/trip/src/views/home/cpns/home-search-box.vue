19b1693942515fbc7f60947cb34d59567c2f20d7
betterme998 <1248715031@qq.com>
2022-09-26 10:55:47 +0800

    抽取home中定位

---
+++ 13项目实战/trip/src/views/home/cpns/home-search-box.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="search-box">
+    <!-- 定位 -->
+    <div class="location">
+      <div class="city">广州</div>
+      <div class="position" @click="positionClick">
+        <span class="text">我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  // 位置/城市
+  const positionClick = () => {
+    // 获取位置方法，传入一个成功的回调，和失败的回调，可选的option
+    // 拿到经纬度后发送给服务器，服务器返回对应城市的名字，我们再展示
+    navigator.geolocation.getCurrentPosition(res => {
+      console.log("获取位置成功：",res);
+    },err => {
+      console.log("获取位置失败：",err);
+    },{
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    }
+  )
+  }
+</script>
+
+<style lang="less" scoped>
+.location {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 20px;
+  .city {
+    flex: 1;
+    color:#333;
+    font-size:15px;
+  }
+  .position {
+    width: 74px;
+    display: flex;
+    align-items: center;
+
+    .text {
+      position: relative;
+      top: 1px;
+      color:#666;
+      font-size:12px;
+    }
+
+    img {
+      margin-left: 5px;
+      width: 18px;
+      height: 18px;
+    }
+  }
+}
+</style>
--- 13项目实战/trip/src/views/home/home.vue
+++ 13项目实战/trip/src/views/home/home.vue
@@ -7,18 +7,14 @@
       <img src="@/assets/img/home/banner.webp"/>
     </div>
     <!-- 定位 -->
-    <div class="location">
-      <div class="city">广州</div>
-      <div class="position">
-        <span class="text">我的位置</span>
-        <img src="@/assets/img/home/icon_location.png" alt="">
-      </div>
-    </div>
+    <home-search-box></home-search-box>
+    
   </div>
 </template>
 
 <script setup>
   import HomeNavBar from './cpns/home-nav-bar.vue'
+  import homeSearchBox from './cpns/home-search-box.vue';
 </script>
 
 <style lang="less" scoped>
@@ -28,30 +24,5 @@
   }
 }
 
-.location {
-  display: flex;
-  align-items: center;
-  height: 44px;
-  padding: 0 20px;
-  .city {
-    flex: 1;
-  }
-  .position {
-    width: 74px;
-    display: flex;
-    align-items: center;
 
-    .text {
-      position: relative;
-      top: 1px;
-      font-size:12px;
-    }
-
-    img {
-      margin-left: 5px;
-      width: 18px;
-      height: 18px;
-    }
-  }
-}
 </style>
