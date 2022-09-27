cf93ee364f902c33028b78218e2fd2f68899d38e
betterme998 <1248715031@qq.com>
2022-09-28 00:31:21 +0800

    抽取数据中的索引，并修改Vant indexBar索引栏右侧索引值，并添加#索引值

--- 13项目实战/trip/src/views/city/cpns/city-group.vue
+++ 13项目实战/trip/src/views/city/cpns/city-group.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="city-group">
     <!-- 通过vant ui -->
-    <van-index-bar>
+    <!-- 抽离数据索引，并替换再加上* -->
+    <van-index-bar :index-list="indexList">
+      <!-- 热门 -->
+      <van-index-anchor index="热门" />
+      <div class="list">
+        <template v-for="(city, index) in groupData.hotCities">
+          <div class="city">{{city.cityName}}</div>
+        </template>
+      </div>
+      
       <template v-for="(group, index) in groupData.cities" :key="index">
         <van-index-anchor :index="group.group" />
         <template v-for="(city, indey) in group.cities" :key="indey">
@@ -25,15 +34,41 @@
 
 <script setup>
 
-defineProps({
+import { computed } from 'vue'
+
+ const props = defineProps({
   groupData: {
     type:Object,
     default: () => ({})
   }
 })
 
+// 处理数据中的索引
+const indexList = computed(() => {
+  const list =  props.groupData.cities.map(item => item.group)
+  list.unshift("#")
+  return list
+})
+
 </script>
 
 <style lang="less" scoped>
-
+  .list {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    padding: 10px;
+    padding-right: 25px;
+    .city {
+      width: 70px;
+      height: 28px;
+      border-radius: 14px;
+      font-size: 12px;
+      color: #000;
+      text-align: center;
+      line-height: 28px;
+      background-color: #fff4ec;
+      margin: 6px 0;
+    }
+  }
 </style>
