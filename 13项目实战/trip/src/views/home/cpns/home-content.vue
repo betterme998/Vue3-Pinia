12b7bb65db9d78ffa0848099f643807123d2811f
betterme998 <1248715031@qq.com>
2022-10-04 23:45:46 +0800

    热门推荐两种展示形式

--- 13项目实战/trip/src/views/home/cpns/home-content.vue
+++ 13项目实战/trip/src/views/home/cpns/home-content.vue
@@ -2,13 +2,24 @@
   <div class="content">
       <h2 class="title">热门推荐</h2>
       <div class="list">
-        列表数据
+        <template v-for="(item, index) in houselist" :key="item.data.houseId">
+          <HouseItemV9 v-if="item.discoveryContentType === 9" :item-data = "item.data"/>
+          <HouseItemV3 v-else-if="item.discoveryContentType === 3" :item-data = "item.data"/>
+        </template>
       </div>
     </div>
 </template>
 
 <script setup>
+// 导入展示不同状态的数组
+import HouseItemV9 from "@/components/house-item-v9/house-item-v9.vue"
+import HouseItemV3 from "@/components/house-item-v3/house-item-v3.vue"
+import useHomeStore from '@/stores/modules/home'
+import { storeToRefs } from 'pinia'
 
+// 拿到home页面的store
+const homeStore = useHomeStore()
+const { houselist } = storeToRefs(homeStore) //拿到推荐数据
 </script>
 
 <style lang="less" scoped>
@@ -18,6 +29,11 @@
   .title {
     font-size: 22px;
     padding: 10px;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
   }
 }
 </style>
