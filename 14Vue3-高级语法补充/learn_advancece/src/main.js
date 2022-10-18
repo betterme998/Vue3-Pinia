739d865672711250a7a69f2701b6fad5c0b550c7
betterme998 <1248715031@qq.com>
2022-10-18 11:23:44 +0800

    安装插件

--- 14Vue3-高级语法补充/learn_advancece/src/main.js
+++ 14Vue3-高级语法补充/learn_advancece/src/main.js
@@ -1,8 +1,31 @@
 import { createApp } from 'vue'
-import App from './02_内置组件补充/App.vue'
+// import App from './01_自定义指令/App.vue'
+// import App from './02_内置组件补充/App.vue'
+import App from './03_安装插件/App.vue'
 import useDirectivs from "./01_自定义指令/directives/index"
 
+// import router from "./router"
 
-const app = createApp(App)
-useDirectivs(app)
-app.mount('#app')
+
+// const app = createApp(App)
+// useDirectivs(app)
+
+// // 安装插件
+// // 方式一：1.传入对象
+// // app.use()可以传入对象,但必须要install：function(){}
+// app.use({
+//   // 会自动执行，并传入app
+//   install: function(app) {
+//     console.log("传入对象的install被执行",app);
+//   }
+// })
+
+// // 方式二：2.传入函数
+// app.use(function(app){
+//   console.log("传入的函数被执行",app);
+// })
+// app.mount('#app')
+
+// use(router) 本质是传入一个对象，会调用router中的install函数
+createApp(App).use(useDirectivs).mount('#app')
+
---
+++ 14Vue3-高级语法补充/learn_advancece/src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter } from "vue-router";
+
+// 创建router
+const router = createRouter({
+  history
+})
+
+export default router
