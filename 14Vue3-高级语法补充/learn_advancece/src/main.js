81724bc8bdc8057c01bbc3e808be25425f0f78c4
betterme998 <1248715031@qq.com>
2022-10-18 23:41:30 +0800

    过渡动画

--- 14Vue3-高级语法补充/learn_advancece/src/main.js
+++ 14Vue3-高级语法补充/learn_advancece/src/main.js
@@ -3,7 +3,8 @@ import { createApp } from 'vue'
 // import App from './02_内置组件补充/App.vue'
 // import App from './03_安装插件/App.vue'
 // import App from './04_Render函数/App.vue'
-import App from './05_JSX的语法/App.vue'
+// import App from './05_JSX的语法/App.vue'
+import App from './06_过渡动画/App.vue'
 import useDirectivs from "./01_自定义指令/directives/index"
 
 // import router from "./router"
