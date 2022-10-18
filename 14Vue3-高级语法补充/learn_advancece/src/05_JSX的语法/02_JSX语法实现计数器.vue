76a7c9f5c69da0ff3e41bfaf81e6f179c9293b72
betterme998 <1248715031@qq.com>
2022-10-18 23:23:26 +0800

    JSX-optionsAPI_Composition-API用法-计数器案例

---
+++ 14Vue3-高级语法补充/learn_advancece/src/05_JSX的语法/02_JSX语法实现计数器.vue
@@ -0,0 +1,36 @@
+<!-- 在options API 中编写jsx -->
+<!-- 指定语言 -->
+<script lang="jsx">
+import about from "./About.vue";
+  export default {
+    data() {
+      return {
+        counter:0
+      }
+    },
+    render() {
+      return (
+        <div class="app">
+          // 通过大括号拿到data,methods等的的值
+          <h2>当前计数：{this.counter}</h2>
+          <button onClick={this.increment}>+1</button>
+          <button onClick={this.decrement}>-1</button>  
+          // 使用组件
+          <about/>
+        </div>
+      )
+    },
+    methods: {
+      increment() {
+        this.counter++
+      },
+      decrement() {
+        this.counter--
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- 14Vue3-高级语法补充/learn_advancece/src/05_JSX的语法/App.vue
+++ 14Vue3-高级语法补充/learn_advancece/src/05_JSX的语法/App.vue
@@ -1,20 +1,68 @@
-<!-- 在options API 中编写jsx -->
+<!-- 在Composition API setup函数中 中编写jsx -->
 <!-- 指定语言 -->
-<script lang="jsx">
-  export default {
-    render() {
-      // 不再编写h()函数,而是编写jsx
-      // 在js里面编写div，这就是jsx （jsx javascript extension 扩展）js扩展，可以在js编写html代码
-      // 注意：这里return的是 （）
-      return (
-        <div class="app">
-          <h2>我是标题</h2>
-          <p>我是内容，哈哈哈</p>  
-        </div>
-      )
+<!-- <script lang="jsx">
+import { ref } from 'vue'
+import about from "./About.vue";
+
+export default {
+  setup() {
+    const counter = ref(0)
+
+    const increment = () => {
+      counter.value++
     }
+    const decrement = () => {
+      counter.value--
+    }
+    
+
+    return () => (
+      <div class="app">
+        // 通过大括号拿到data,methods等的的值
+        <h2>当前计数：{counter.value}</h2>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>  
+        // 使用组件
+        <about/>
+      </div>
+    )
   }
+}
+</script> -->
+
+
+<!-- Composition API setup语法糖 jsx写法 -->
+<template>
+  <!-- setup语法糖 jsx写法 -->
+  <jsx/>
+</template>
+
+<script lang="jsx" setup>
+import { ref } from 'vue'
+import about from "./About.vue";
+
+  const counter = ref(0)
+
+  const increment = () => {
+    counter.value++
+  }
+  const decrement = () => {
+    counter.value--
+  }
+  
+  const jsx = () => (
+    <div class="app">
+      // 通过大括号拿到data,methods等的的值
+      <h2>当前计数：{counter.value}</h2>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>  
+      // 使用组件
+      <about/>
+    </div>
+  )
+
 </script>
+
 
 <style lang="less" scoped>
 
