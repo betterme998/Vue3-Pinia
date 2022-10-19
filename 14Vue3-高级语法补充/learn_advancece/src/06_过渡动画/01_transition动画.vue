0268796bcc7770a61010101c896a4aa9e554e0f1
betterme998 <1248715031@qq.com>
2022-10-19 12:07:51 +0800

    transition内置组件-transition动画-animation动画

---
+++ 14Vue3-高级语法补充/learn_advancece/src/06_过渡动画/01_transition动画.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app">
+    <!-- 点击切换和隐藏，添加动画 -->
+    <!-- 
+      我们点击后需要动画，就要添加类，手动添加比较繁琐
+      vue为我们添加了内置组件<transition>
+     -->
+    <div>
+      <button @click="isShow = !isShow">切换</button>
+    </div>
+
+    <!-- 内置组件 transition  在合适的时机加上类-->
+    <transition>
+      <h2 v-if="isShow">哈哈哈</h2>
+    </transition>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const isShow = ref(false)
+</script>
+
+<style scoped>
+h2 {
+  display: inline-block;
+}
+
+/* 离开结束 */
+.v-leave-to,
+/* 进入开始 */
+.v-enter-from {
+  opacity: 0;
+  transform: scale(0.6);
+}
+
+/* 离开开始 */
+.v-leave-from,
+/* 进入结束 */
+.v-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+
+
+
+.v-enter-active,
+.v-leave-active{
+  transition: all 2s ease;
+}
+</style>
